53f37c4032ece4c45f874d8f6fc467aa631c1e33
Ubuntu <ubuntu@ip-172-31-31-42.ec2.internal>
2019-09-13 18:17:22 +0000

    My stylish hello world program

---
+++ hello_world_stylish.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Joseph Palermo
+# Created on: September 2019
+# This is the "Hello, World!" program, but stylish
+
+
+def main():
+    print ("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
